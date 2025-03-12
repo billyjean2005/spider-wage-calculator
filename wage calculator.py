9e2cb10bc7d793537c04cf5e69778f5b5b99463e
Samantha <165994286+billyjean2005@users.noreply.github.com>
2025-03-12 14:38:42 +0200

    Add files via upload

---
+++ wage calculator.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+# variable_declaration 
+number_of_hours=32
+hourly_rate=200
+total_wage=0
+
+# calculating total wage 
+
+total_wage=hourly_rate*number_of_hours 
+
+# displaying results 
+
+print('gross wage;',total_wage)
+
+
