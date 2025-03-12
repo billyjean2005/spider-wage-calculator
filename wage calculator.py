69488602eb8252043dbcd2f5c15660840d41bf28
Samantha <165994286+billyjean2005@users.noreply.github.com>
2025-03-12 14:59:13 +0200

    Add files via upload

--- wage calculator.py
+++ wage calculator.py
@@ -8,6 +8,9 @@ This is a temporary script file.
 number_of_hours=32
 hourly_rate=200
 total_wage=0
+overtime_wage=0
+overtime_hours=12
+
 
 # calculating total wage 
 
@@ -16,5 +19,16 @@ total_wage=hourly_rate*number_of_hours
 # displaying results 
 
 print('gross wage;',total_wage)
+
+# calculating overtime wage
+overtime_wage=overtime_hours*2*hourly_rate 
+# calculating total wage 
+
+total_wage=hourly_rate*number_of_hours + overtime_wage
+
+#displaying results
+print('overtime wage;',overtime_wage)
+print('gross wage;', total_wage)
+
 
 
